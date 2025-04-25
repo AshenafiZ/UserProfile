0ebb6a008e02685bda1a7642ce1b8dd92e591fa2
Ashenafi Zewdie <121452448+AshenafiZ@users.noreply.github.com>
2025-04-26 01:19:30 +0300

    Login Sucessful

--- flutter_app/lib/services/api_service.dart
+++ flutter_app/lib/services/api_service.dart
@@ -2,30 +2,43 @@ import 'package:dio/dio.dart';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:flutter/foundation.dart';
+import 'package:dio/browser.dart';
 
 class ApiService {
   final Dio dio = Dio();
   final cookieJar = CookieJar();
 
   ApiService() {
-    dio.options.baseUrl = kIsWeb ? 'http://localhost:5000/api'  : 'http://10.0.2.2:5000/api ';
+    dio.options.baseUrl = kIsWeb ? 'http://localhost:5000'  : 'http://10.0.2.2:5000';
     dio.options.headers['Content-Type'] = 'application/json';
     if(!kIsWeb){
       dio.interceptors.add(CookieManager(cookieJar));
     }
+    // if (kIsWeb) {
+    //   dio.httpClientAdapter = BrowserHttpClientAdapter()
+    //     ..withCredentials = true;
+    // }
   }
 
-  Future<Response> get(String path, {Map<String, dynamic>? query}) async {
-    return await dio.get(path);
+  Future<Response> get(String path, {Map<String, dynamic>? query}) {
+    return dio.get(path, queryParameters: query);
   }
 
-  Future<Response> post(String path, {Map<String, dynamic>? data}) async {
-    return await dio.post(path, data: data);
+  Future<Response> post(String path, {Map<String, dynamic>? data}) {
+    return dio.post(path, data: data);
   }
 
-  Future<Response> postFormData(String path, FormData formData) async {
-    return await dio.post(path, data: formData);
+  Future<Response> put(String path, {Map<String, dynamic>? data}) {
+    return dio.put(path, data: data);
+  }
+
+  Future<Response> delete(String path, {Map<String, dynamic>? data}) {
+    return dio.delete(path, data: data);
+  }
+
+  Future<Response> postFormData(String path, FormData formData) {
+    return dio.post(path, data: formData);
   }
 }
 
-final apiService = ApiService(); // Exported instance
+final apiService = ApiService(); 
